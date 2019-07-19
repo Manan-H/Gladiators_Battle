87de11e717f6336d69908884324ca4ae39c60225
Manan-H <manan.hambardzumyan@gmail.com>
2019-07-19 11:23:58 +0400

    task completed

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 class Gladiator{
 
 	constructor(health, power, speed, name){
@@ -15,8 +14,8 @@ let gladiators = [];
 
 for(let i = 0; i < 5; i++) {
 	let health = Math.floor(Math.random() * (100 - 80 + 1) ) + 80;
-	let power = Math.floor(Math.random() * (5 - 2 + 1) ) + 2;
-	let speed = Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
+	let power = (Math.random() * (5 - 2 + 1) + 1).toFixed(1);
+	let speed = (Math.random() * (5 - 1 + 1)).toFixed(3);
 	let name = faker.name.firstName();
 
 	let gladiator = new Gladiator(health, power, speed, name);
@@ -29,8 +28,25 @@ console.log(gladiators);
 
 
 
-
 function hit(gladiatorIndex){
+
+	let opponentIndex = findOpponent(gladiatorIndex);
+	let gladiator = gladiators[gladiatorIndex];
+	let opponent = gladiators[opponentIndex];
+	let initialHealth = opponent.health;
+
+	opponent.health = opponent.health - gladiator.power;
+	updateSpeed(opponent, initialHealth);
+
+	let msg = `${gladiator.name} hits ${opponent.name} with power ${gladiator.power}`;
+	console.log(msg);
+	showDom(msg);
+
+	return checkHealth(opponentIndex)
+		
+}
+
+function findOpponent(gladiatorIndex){
 
 	let opponentIndex = Math.floor(Math.random() * gladiators.length );
 
@@ -38,32 +54,23 @@ function hit(gladiatorIndex){
 		opponentIndex = Math.floor(Math.random() * gladiators.length);
 	}
 
-	let opponent = gladiators[opponentIndex];
-	let gladiator = gladiators[gladiatorIndex];
-
-	opponent.health = opponent.health - gladiator.power;
-	console.log(`${gladiator.name} hits ${opponent.name} with power ${gladiator.power}`);
-
-	return checkHealth(opponentIndex)
-		
+	return opponentIndex;
 }
+
 
 
 function checkHealth(opponentIndex){
 
+	let gladiator = gladiators[opponentIndex];
 
-		let gladiator = gladiators[opponentIndex];
-
-		if(gladiator.health <=0){
-				console.log(`${gladiator.name} is dying`);
-				return caesarsDecision(opponentIndex);
-			}else if(gladiator.health > 15 && gladiator.health < 30){
-				gladiators[opponentIndex].speed = gladiators[opponentIndex].speed * 3
-				return true;
-			}else{
-				return true;
-			}
-
+	if(gladiator.health <=0){
+			let msg = `${gladiator.name} is dying`;
+			console.log(msg);
+			showDom(msg);
+			return caesarsDecision(opponentIndex);
+		}else if(gladiator.health > 15 && gladiator.health < 30){
+			gladiators[opponentIndex].speed = gladiators[opponentIndex].speed * 3
+		}
 }
 
 
@@ -74,91 +81,81 @@ function caesarsDecision(dyingGladiatorIndex){
 	let dyingGladiator = gladiators[dyingGladiatorIndex];
 
 		if(caesarsDecision[option] == 'Live'){
-			dyingGladiator.health += 50;
-			console.log(`Caesar showed thumbs up to ${dyingGladiator.name}`);
-			return true;
+			healthRecovered(dyingGladiator);
+
 		}else {
-			gladiators.splice(dyingGladiatorIndex, 1);
-			console.log(`Caesar showed &#128078 to ${dyingGladiator.name}`);
+			gladiatorDies(dyingGladiatorIndex);
+			
 			if(gladiators.length == 1){
-				console.log(`The winner is ${gladiators[0].name}`);
+				winnerIs();	
 			}
-			return false;
 	}	
 
+}
+
+function gladiatorDies(dyingGladiatorIndex){
+	let msg = `Caesar showed thumbs down to ${gladiators[dyingGladiatorIndex].name}`;
+	console.log(msg);
+	showDom(msg);
+
+	gladiators.splice(dyingGladiatorIndex, 1);
+}
+
+function healthRecovered(dyingGladiator){
+
+	dyingGladiator.health += 50;
+
+	let msg = `Caesar showed thumbs up to ${dyingGladiator.name}`;
+	console.log(msg);
+	showDom(msg);
+}
+
+function winnerIs(){
+	let msg = `The winner is ${gladiators[0].name}`;
+	console.log(msg);
+	showDom(msg);
+}
+
+
+function updateSpeed(gladiator, initialHealth){
+	gladiator.speed = gladiator.speed * (gladiator.health/initialHealth);
 } 
 
 
 function start(){
 
-
 	for(let i = 0; i < gladiators.length; i++){
 
-		let frequency = gladiators[i].speed;
-
-		switch(frequency) {
-  			case 1:
-      		frequency = 5000;
-    		break;
-  			case 2:
-   			 frequency = 4000;
-    		break;
-    		case 3:
-   			 frequency = 3000;
-    		break;
-    		case 4:
-   			 frequency = 2000;
-    		break;
-    		case 5:
-   			 frequency = 1000;
-    		break;
-  			default:
-    		5000
-		}
-
-		fight(i, frequency);
-
+		fight(i);
 	}
-
 }
 
 
-function fight(i, frequency){
+function fight(i){
 
-	//works with setInterval
+	//setInterval(hit, frequency, i);
 
-	setInterval(hit, frequency, i);
+	let gladiatorIndex = i;
+	let frequency = 6000 - (gladiators[gladiatorIndex].speed * 1000);
 
-	//with promise not yet
+	hit(gladiatorIndex);
 
 
-	// let startFight = (i, frequency) => {
- //      return new Promise((resolve, reject) => {
- //        let interval = setInterval(() => {
- //          hit(i)
- //            .then((data) => {
- //                 if (!data) {
- //                 	clearInterval(interval);
- //               } else if (data) {
- //                   resolve()
- //               }
- //           });
- //        }, frequency);
- //      });
- //    };
+	setTimeout((gladiatorIndex) => {
+
+		if(gladiators[i] && gladiators.length > 1){
+
+			fight(i);
+		}
+		
+	}, frequency);
 	
 }
 
+function showDom(msg) {
+  let msgLine = document.createElement("P");
+  msgLine.innerText = msg;
+  document.body.appendChild(msgLine);
+}
+
 start();
-
-
-
-
-
-
-
-
-
-
-
-
